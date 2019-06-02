32d426863a84a148cdc317dd3e42b26b4e41e997
Emily Brown <emily_brown@rogers.com>
2019-06-02 12:49:58 -0400

    created compare letter function and scorboard functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,7 +25,7 @@ function gameStart() {
     blanks = wordLetters.length;
 
     // Push each letter from the newly created random word split array by using a loop to push the underscore blank spot
-    for (let i = 0, i < blanks, i++){
+    for (let i = 0; i < blanks; i++){
         blanksCorrect.push("_");
     }
 
@@ -40,3 +40,55 @@ function gameReset() {
     blanksCorrect = [];
     gameStart()
 }
+
+// Create comparison for letters entered by player against the actual word generated from wordbank 
+
+// Create comparison function 
+function compareLetters(letter) {
+    let letterOfWord = false;
+    for (let i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterOfWord = true;
+        }
+    }
+
+    if (letterOfWord){
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksCorrect[i] = letter;
+            }
+        }
+    }
+
+    else{
+        wrongGuess.push(letter);
+        remainingGuesses--;
+
+    }
+}
+
+// Create function for the scoreboard - checking/comparing wins against losses 
+function scoreboard() {
+    if (wordLetters.toString() == blanksCorrect.toString()){
+        wins++;
+        gameReset()
+        document.getElementById("wins").innerHTML = " " + wins;
+    }
+    
+    else if (remainingGuesses === 0) {
+        losses++;
+        gameReset()
+        document.getElementById("loss").innerHTML = " " + losses;
+    }
+}
+
+// Create event to check and store player letter guess 
+document.onkeyup = function (event) {
+    let guess = String.fromCharCode(event.keyCode).toLowerCase();
+    compareLetters(guesses);
+    scoreboard();
+    document.getElementById("lettersGuessed").innerHTML = " " + wrongGuess.join(" ");
+}
+
+// Start Game
+gameStart();

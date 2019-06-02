aee5473ba603e28ce7a50fb152d75d4552ae807c
Emily Brown <emily_brown@rogers.com>
2019-06-02 11:42:54 -0400

    created Game Start function to pull random word from word bank and then break that word up into it's seperate letter and put those letters into its own array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ let wordBank = ["octopus", "mermaid", "seahorse", "whale", "shark"]
 
 // Create the empty variables
 let randomWord = "";
-let lettersInWord = [];
+let wordLetters = [];
 let blanks = 0;
 let blanksCorrect = [];
 let wrongGuess =[];
@@ -12,4 +12,18 @@ let wrongGuess =[];
 function gameStart () {
     // Choose random word from word bank
     randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    // Split letters of word up for player to guess each letter in new array
+    wordLetters = randomWord.split("");
+
+    // Break the length of the word up into the blanks 
+    blanks = wordLetters.length;
+
+    // Push each letter from the newly created random word split array by using a loop to push the underscore blank spot
+    for (let i = 0, i < blanks, i++){
+        blanksCorrect.push("_");
+    }
+
+    // Get the blanks to push through to the html document 
+    document.getElementById("wordGuess").innerHTML = "  " + blanksCorrect.join("  ")
 }
